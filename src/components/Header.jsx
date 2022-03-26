b468a777ee3aba0d8e4373142af1ba9e4d0d8d82
Karthik <kartik.kotari@gmail.com>
2022-03-26 13:35:55 +0100

    3.1.1 Code Cleanup

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,13 +4,13 @@ class Header extends Component {
   state = {};
   render() {
     return (
-      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
             Chuck Norris Jokes
           </a>
           <button
-            class="navbar-toggler"
+            className="navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#navbarColor02"
@@ -18,21 +18,20 @@ class Header extends Component {
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <span class="navbar-toggler-icon"></span>
+            <span className="navbar-toggler-icon"></span>
           </button>
 
-          <div class="collapse navbar-collapse" id="navbarColor02">
-            <ul class="navbar-nav me-auto">
-              <li class="nav-item">
-                <a class="nav-link active" href="#">
+          <div className="collapse navbar-collapse" id="navbarColor02">
+            <ul className="navbar-nav me-auto">
+              <li className="nav-item">
+                <a className="nav-link active" href="#">
                   Home
-                  <span class="visually-hidden">(current)</span>
                 </a>
               </li>
             </ul>
-            <form class="d-flex">
+            <form className="d-flex">
               <input
-                class="form-control me-sm-2"
+                className="form-control me-sm-2"
                 type="text"
                 placeholder="Search.."
               />
