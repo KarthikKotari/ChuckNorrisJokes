fb95dc07ae06cd8436a3eb83e375f52afbe9b38b
Karthik <kartik.kotari@gmail.com>
2022-03-26 14:41:15 +0100

    3.2 Fetching data dynamically - Added Axios libraries

--- src/components/ChucksHome.jsx
+++ src/components/ChucksHome.jsx
@@ -1,34 +1,34 @@
 import React, { Component } from "react";
 import ChucksCategory from "./ChucksCategory";
+import Axios from "axios";
 
 class ChucksHome extends Component {
   state = {
-    categories: [
-      "animal",
-      "career",
-      "celebrity",
-      "dev",
-      "explicit",
-      "fashion",
-      "food",
-      "history",
-      "money",
-      "movie",
-      "music",
-      "political",
-      "religion",
-      "science",
-      "sport",
-      "travel",
-    ],
+    categories: [],
+  };
+
+  //Lifecycle function is called exactly once initially
+  componentDidMount() {
+    this.fetchCategories();
+  }
+  // Fetching categories
+  fetchCategories = () => {
+    Axios.get(`https://api.chucknorris.io/jokes/categories`)
+      .then((resp) => resp.data) //When there is a response, give resp.data
+      .then((categories) => this.setState({ categories }));
   };
   render() {
-    const categoryList = this.state.categories.map((category) => (
-      <ChucksCategory jokeCategory={category} />
+    const categoryList = this.state.categories.map((category, index) => (
+      <ChucksCategory jokeCategory={category} key={index} />
     ));
+
     return (
       <div>
-        <img src="./images/chuck.png" alt="" className="card-img-top"></img>
+        <img
+          src="./images/chuck.png"
+          alt=""
+          className="mx-auto d-block col-md-6"
+        ></img>
         <br></br>
         <div className="row">{categoryList}</div>
       </div>
